ee3b95cd4514a05a586c7de1fe3981c8548b896c
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-08-14 13:20:07 +0900

    feat: [윤주아] 네비게이션 바 생성 [RDY-1]

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { AuthProvider } from "navigation/AuthConText";
+import Header from "navigation/Header";
 import MainPage from "page/MainPage";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <>
+    <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-        </Routes>
+        <Header>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+          </Routes>
+        </Header>
       </BrowserRouter>
-    </>
+    </AuthProvider>
   );
 };
 
---
+++ src/navigation/AuthConText.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useCallback, useContext, useEffect, useState } from 'react';
+
+
+type AuthContextType = {
+  isLoggedIn: boolean;
+  setIsLoggedIn: (isLoggedIn: boolean) => void;
+  checkAuthorization: () => boolean;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const checkAuthorization = useCallback((): boolean => {
+    const accessToken = localStorage.getItem('accessToken');
+    return !!accessToken;
+  }, []); 
+
+  useEffect(() => {
+    setIsLoggedIn(checkAuthorization());
+  }, [checkAuthorization]);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, checkAuthorization }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextType => {
+  const auth = useContext(AuthContext);
+  if (!auth) {
+    throw new Error('AuthProvider밖에서 사용할 수 없습니다.');
+  }
+  return auth;
+};
