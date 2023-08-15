1c5acf25e14a1cbaaaea130cd3c2827b7e2d035b
dbswndk <128335528+dbswndk@users.noreply.github.com>
2023-08-15 17:42:22 +0900

    style: [윤주아] 헤더 텍스트 버튼 이미지 버튼으로 수정 [RDY-1]

--- src/layout/navigation/Header.tsx
+++ src/layout/navigation/Header.tsx
@@ -1,7 +1,7 @@
-import React, { ReactNode } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "./AuthConText";
-import "./css/Header.css";
+import React, { ReactNode } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from './AuthConText';
+import './css/Header.css'
 
 type HeaderProps = {
   children?: ReactNode;
@@ -11,35 +11,30 @@ const Header: React.FC<HeaderProps> = ({ children }) => {
   const navigate = useNavigate();
   const { isLoggedIn, setIsLoggedIn } = useAuth();
 
-  const handleLogout = () => {};
+  const handleLogout = () => {
+    
+  };
 
   return (
     <div>
-      <div className="top-bar">
-        <div className="menu-container">
-          <Link className="Menu" to={"/"}>
-            home
-          </Link>
+      <div className='top-bar'>
+        <Link className='Home' to={'/'}>TOTO마켓</Link>
           {isLoggedIn ? (
             <>
-              <button className="Menu-logout" onClick={handleLogout}>
-                로그아웃
-              </button>
-              <Link className="Menu" to={"/myPage"}>
-                마이페이지
+              <Link className='MyPage' to={'/myPage'}>
+              <img src='img/MyPage.png' alt='마이페이지' />
               </Link>
+              <button className='Menu-logout' onClick={handleLogout}>
+                <img src='img/Logout.png' alt='로그아웃' />
+              </button>
             </>
           ) : (
             <>
-              <Link className="Menu" to={"/login"}>
-                로그인
-              </Link>
-              <Link className="Menu" to={"/signup"}>
-                회원가입
+              <Link className='Login' to={'/login'}>
+                <img src='img/Login.png' alt='로그인' />
               </Link>
             </>
           )}
-        </div>
       </div>
       {children}
     </div>
