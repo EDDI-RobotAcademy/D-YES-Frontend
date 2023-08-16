311e6eb0a4f4c9bd760106751b43897c0c14e36e
dbswndk <dbswndk123@gmail.com>
2023-08-16 09:39:36 +0900

    update: [윤주아] 로그인을 성공하면 홈페이지로 이동합니다.[RDY-10]

--- src/layout/navigation/AuthConText.tsx
+++ src/layout/navigation/AuthConText.tsx
@@ -23,6 +23,8 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     if (userToken) {
       localStorage.setItem('userToken', userToken);
       setIsLoggedIn(true);
+  
+      window.location.href = "/";
     }
   }, []);
 
