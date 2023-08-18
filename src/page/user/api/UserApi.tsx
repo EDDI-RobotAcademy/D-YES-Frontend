a3bd9b211855eda90e1d126f4188e7c6f20f7089
bbbii <hyeongjin1326@gmail.com>
2023-08-18 16:13:43 +0900

    update: [김형진] 로그아웃 요청 시 예외처리 추가 [RDY-11]

--- src/page/user/api/UserApi.tsx
+++ src/page/user/api/UserApi.tsx
@@ -81,9 +81,10 @@ export const checkNicknameDuplicate = async (nickName: string) => {
 
 // 회원 로그아웃
 export const userLogout = async () => {
-  await axiosInstance.springAxiosInst.get(`/user/logOut`, {
+  const response = await axiosInstance.springAxiosInst.get(`/user/logOut`, {
     params: {
       userToken: userToken,
     },
   });
+  return response.data;
 };
