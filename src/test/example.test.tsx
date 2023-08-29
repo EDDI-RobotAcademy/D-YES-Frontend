8d85d22bb40844d75fb82625bbae766f5805d48f
jeongdawun <jeongdawun33@gmail.com>
2023-08-29 12:26:10 +0900

    feat: [정다운] 메인페이지에 상품 목록 페이지로 이동하는 버튼 추가 [RDY-41]

--- src/test/example.test.tsx
+++ src/test/example.test.tsx
@@ -3,9 +3,14 @@ import '@testing-library/jest-dom';
 import "matchmedia-polyfill";
 import "matchmedia-polyfill/matchMedia.addListener";
 import { render } from "@testing-library/react";
+import { HashRouter } from 'react-router-dom';
 import MainPage from "../page/MainPage";
 
 
 it("메인페이지 테스트", () => {
-    render(<MainPage />);
+    render(
+        <HashRouter>
+            <MainPage />
+        </HashRouter>
+    );
 });
