ba867f7f247d85b26065b697326362835f3c1963
bbbii <hyeongjin1326@gmail.com>
2023-09-03 21:57:25 +0900

    feat: [김형진] 상품 상세보기 데이터 조건부 렌더링 처리 [RDY-37]

--- src/page/product/productUser/ProductDetail.tsx
+++ src/page/product/productUser/ProductDetail.tsx
@@ -35,12 +35,12 @@ const ProductDetailPage = () => {
   const { data } = useProductDetailQuery(productId || "");
   const [productQuantity, setProductQuantity] = useState(1);
   const [selectedOptionId, setSelectedOptionId] = useState<number | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchProductData = async () => {
       const data = await getProductDetail(productId || "");
-      setIsLoading(false);
+      setIsLoading(true);
       if (data?.optionList && data.optionList.length > 0) {
         setSelectedOptionId(data.optionList[0].optionId!);
       }
@@ -90,117 +90,123 @@ const ProductDetailPage = () => {
     <div className="product-detail-container">
       <div className="product-datail-main">
         <div className="product-info-grid">
-          <div style={{ overflow: "hidden" }}>
-            <div className="product-main-image">
-              <div className="product-details">
-                <Carousel images={imageArray} />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div>
-              <div className="product-name">{data?.productResponse.productName}</div>
-              <div className="spacer-2" />
-              <div className="product-option-price">
-                {selectedOption ? won(selectedOption.optionPrice) : ""}
-              </div>
-              <div className="spacer-3" />
-              {data?.productResponse.cultivationMethod &&
-                data?.productResponse.cultivationMethod.length > 0 && (
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "flex-start",
-                      gap: "4px",
-                      marginTop: "8px",
-                    }}
-                  >
-                    <div
-                      data-testid="cultivation-method"
-                      className={`${
-                        tagMapping[data?.productResponse.cultivationMethod]?.className
-                      } tag-common`}
-                    >
-                      {tagMapping[data?.productResponse.cultivationMethod]?.name ||
-                        data?.productResponse.cultivationMethod}
-                    </div>
+          {data && isLoading ? (
+            <>
+              <div style={{ overflow: "hidden" }}>
+                <div className="product-main-image">
+                  <div className="product-details">
+                    <Carousel images={imageArray} />
                   </div>
-                )}
-            </div>
-            <hr className="hr-style" />
-            <div className="spacer-3" />
-            <FormControl fullWidth>
-              <div className="choose-option">옵션을 선택해주세요</div>
-              <Select
-                value={selectedOptionId !== null ? selectedOptionId.toString() : ""}
-                onChange={(e) => setSelectedOptionId(parseInt(e.target.value))}
-                style={{ fontFamily: "SUIT-Medium" }}
-              >
-                {data?.optionList.map((option, idx) => (
-                  <MenuItem
-                    key={idx}
-                    value={option.optionId.toString()}
-                    disabled={option.stock === 0}
-                    style={{ fontFamily: "SUIT-Medium" }}
-                  >
-                    {option.stock === 0 && (
-                      <span
+                </div>
+              </div>
+              <div>
+                <div>
+                  <div className="product-name">{data?.productResponse.productName}</div>
+                  <div className="spacer-2" />
+                  <div className="product-option-price">
+                    {selectedOption ? won(selectedOption.optionPrice) : ""}
+                  </div>
+                  <div className="spacer-3" />
+                  {data?.productResponse.cultivationMethod &&
+                    data?.productResponse.cultivationMethod.length > 0 && (
+                      <div
                         style={{
-                          color: "red",
-                          marginRight: "5px",
-                          fontFamily: "SUIT-Bold",
                           display: "flex",
+                          justifyContent: "flex-start",
+                          gap: "4px",
+                          marginTop: "8px",
                         }}
                       >
-                        품절
-                      </span>
+                        <div
+                          data-testid="cultivation-method"
+                          className={`${
+                            tagMapping[data?.productResponse.cultivationMethod]?.className
+                          } tag-common`}
+                        >
+                          {tagMapping[data?.productResponse.cultivationMethod]?.name ||
+                            data?.productResponse.cultivationMethod}
+                        </div>
+                      </div>
                     )}
-                    <div className="options-style">
-                      {option.optionName + " / " + option.value + option.unit}
-                      <span>{won(option.optionPrice)}</span>
+                </div>
+                <hr className="hr-style" />
+                <div className="spacer-3" />
+                <FormControl fullWidth>
+                  <div className="choose-option">옵션을 선택해주세요</div>
+                  <Select
+                    value={selectedOptionId !== null ? selectedOptionId.toString() : ""}
+                    onChange={(e) => setSelectedOptionId(parseInt(e.target.value))}
+                    style={{ fontFamily: "SUIT-Medium" }}
+                  >
+                    {data?.optionList.map((option, idx) => (
+                      <MenuItem
+                        key={idx}
+                        value={option.optionId.toString()}
+                        disabled={option.stock === 0}
+                        style={{ fontFamily: "SUIT-Medium" }}
+                      >
+                        {option.stock === 0 && (
+                          <span
+                            style={{
+                              color: "red",
+                              marginRight: "5px",
+                              fontFamily: "SUIT-Bold",
+                              display: "flex",
+                            }}
+                          >
+                            품절
+                          </span>
+                        )}
+                        <div className="options-style">
+                          {option.optionName + " / " + option.value + option.unit}
+                          <span>{won(option.optionPrice)}</span>
+                        </div>
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+                <div className="spacer-3" />
+                <div className="product-quantity">
+                  총 상품 가격
+                  <span className="product-price-sum">
+                    <div className="product-counter-button">
+                      <div className="product-button" onClick={handleDecreaseQuantity}>
+                        <RemoveIcon />
+                      </div>
+                      <span className="product-counter">{productQuantity}</span>
+                      <div className="product-button" onClick={handleIncreaseQuantity}>
+                        <AddIcon />
+                      </div>
                     </div>
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-            <div className="spacer-3" />
-            <div className="product-quantity">
-              총 상품 가격
-              <span className="product-price-sum">
-                <div className="product-counter-button">
-                  <div className="product-button" onClick={handleDecreaseQuantity}>
-                    <RemoveIcon />
-                  </div>
-                  <span className="product-counter">{productQuantity}</span>
-                  <div className="product-button" onClick={handleIncreaseQuantity}>
-                    <AddIcon />
-                  </div>
+                    <div className="price-sum">
+                      {selectedOption ? won(selectedOption.optionPrice * productQuantity) : 0}
+                    </div>
+                  </span>
                 </div>
-                <div className="price-sum">
-                  {selectedOption ? won(selectedOption.optionPrice * productQuantity) : 0}
+                <div>
+                  <Button
+                    style={{ fontFamily: "SUIT-Bold", fontSize: "large" }}
+                    className="confirm-button"
+                    variant="outlined"
+                  >
+                    장바구니에 담기
+                  </Button>
                 </div>
-              </span>
-            </div>
-            <div>
-              <Button
-                style={{ fontFamily: "SUIT-Bold", fontSize: "large" }}
-                className="confirm-button"
-                variant="outlined"
-              >
-                장바구니에 담기
-              </Button>
-            </div>
-            <div className="spacer-1" />
-            <div>
-              <Button
-                style={{ fontFamily: "SUIT-Bold", fontSize: "large" }}
-                className="confirm-button"
-                variant="contained"
-              >
-                구매하기
-              </Button>
-            </div>
-          </div>
+                <div className="spacer-1" />
+                <div>
+                  <Button
+                    style={{ fontFamily: "SUIT-Bold", fontSize: "large" }}
+                    className="confirm-button"
+                    variant="contained"
+                  >
+                    구매하기
+                  </Button>
+                </div>
+              </div>
+            </>
+          ) : (
+            <p>로딩중</p>
+          )}
         </div>
 
         <div className="product-description-container">
@@ -222,41 +228,53 @@ const ProductDetailPage = () => {
           <div className="example1" ref={yDetail}>
             <div className="example-container">
               상세 정보
-              <div>
-                {/* 여기에 상세 정보 작성하세요 */}
-                <div className="product-details">
-                  <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 650 }} aria-label="farm-info table">
-                      <TableHead>
-                        <TableRow>
-                          <TableCell>상호명</TableCell>
-                          <TableCell>전화</TableCell>
-                          <TableCell>주소</TableCell>
-                          <TableCell>소개</TableCell>
-                          <TableCell>분류</TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        <TableRow key={data?.farmInfoResponse.farmName}>
-                          <TableCell>{data?.farmInfoResponse.farmName}</TableCell>
-                          <TableCell>{data?.farmInfoResponse.csContactNumber}</TableCell>
-                          {/* 임시로 주소만 표기 */}
-                          <TableCell>{data?.farmInfoResponse.farmAddress.address}</TableCell>
-                          <TableCell>{data?.farmInfoResponse.introduction}</TableCell>
-                          <TableCell>{data?.farmInfoResponse.produceTypes}</TableCell>
-                        </TableRow>
-                      </TableBody>
-                    </Table>
-                  </TableContainer>
-                  <div className="product-description">상품 설명:</div>
-                </div>
-              </div>
+              {data && isLoading ? (
+                <>
+                  <div>
+                    {/* 여기에 상세 정보 작성하세요 */}
+                    <div className="product-details">
+                      <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="farm-info table">
+                          <TableHead>
+                            <TableRow>
+                              <TableCell>상호명</TableCell>
+                              <TableCell>전화</TableCell>
+                              <TableCell>주소</TableCell>
+                              <TableCell>소개</TableCell>
+                              <TableCell>분류</TableCell>
+                            </TableRow>
+                          </TableHead>
+                          <TableBody>
+                            <TableRow key={data?.farmInfoResponse.farmName}>
+                              <TableCell>{data?.farmInfoResponse.farmName}</TableCell>
+                              <TableCell>{data?.farmInfoResponse.csContactNumber}</TableCell>
+                              {/* 임시로 주소만 표기 */}
+                              <TableCell>{data?.farmInfoResponse.farmAddress.address}</TableCell>
+                              <TableCell>{data?.farmInfoResponse.introduction}</TableCell>
+                              <TableCell>{data?.farmInfoResponse.produceTypes}</TableCell>
+                            </TableRow>
+                          </TableBody>
+                        </Table>
+                      </TableContainer>
+                      <div className="product-description">상품 설명:</div>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <p>상품 상세 정보 불러오는 중</p>
+              )}
             </div>
           </div>
 
           <div className="example2" ref={yReview}>
-            {/* 여기에 리뷰 작성하세요 */}
-            <div>여기부터 리뷰</div>
+            {data && isLoading ? (
+              <>
+                {/* 여기에 리뷰 작성하세요 */}
+                <div>여기부터 리뷰</div>
+              </>
+            ) : (
+              <p>리뷰 정보 불러오는 중</p>
+            )}
           </div>
         </div>
       </div>
