ded26858ee65311090e820f050bac42849d033e4
bbbii <hyeongjin1326@gmail.com>
2023-09-07 15:53:49 +0900

    style: [김형진] Header 스타일 수정 [RDY-88]

--- src/layout/navigation/Header2nd.tsx
+++ src/layout/navigation/Header2nd.tsx
@@ -1,6 +1,6 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { Link } from "react-router-dom";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
 import "./css/Header2nd.css";
 
 type HeaderProps = {
@@ -9,24 +9,26 @@ type HeaderProps = {
 
 const Header: React.FC<HeaderProps> = ({ children }) => {
   return (
-    <div className="second-header-fontstyle">
-      <div className="second-top-bar">
-        <div className="top-bar-left">
-          <Link className="go-to-catagory"style={{ display: 'flex', alignItems: 'center' }}  to={"/productList"}>
-            <MenuIcon />
-            <p style={{ marginLeft: '5px' }}>카테고리</p>
-          </Link>
-        </div>
-        <div className="top-bar-right">
-          <Link className="go-to-new" to={"/"}>
-            신상품
-          </Link>
-          <Link className="go-to-best" to={"/"}>
-            베스트
-          </Link>
-          <Link className="go-to-event" to={"/"}>
-            이벤트
-          </Link>
+    <div className="second-header-container">
+      <div className="second-header-fontstyle">
+        <div className={"second-header-top-bar"}>
+          <div className="second-header-btn-container">
+            <Link className="second-header-link" to={"/productList"}>
+              <MenuIcon />
+              <p>카테고리</p>
+            </Link>
+          </div>
+          <div className="second-header-btn-container">
+            <Link className="second-header-link" to={"/"}>
+              신상품
+            </Link>
+            <Link className="second-header-link" to={"/"}>
+              베스트
+            </Link>
+            <Link className="second-header-link" to={"/"}>
+              이벤트
+            </Link>
+          </div>
         </div>
       </div>
       {children}
