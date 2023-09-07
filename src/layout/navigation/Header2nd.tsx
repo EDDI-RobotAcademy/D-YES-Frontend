1934e8296226bcdabecf1f8bebd58d1119bc5e50
bbbii <hyeongjin1326@gmail.com>
2023-09-07 16:01:46 +0900

    feat: [김형진] Header에 마우스를 오버하면 카테고리 출력 [RDY-80]

--- src/layout/navigation/Header2nd.tsx
+++ src/layout/navigation/Header2nd.tsx
@@ -2,18 +2,33 @@ import React, { ReactNode, useState } from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import "./css/Header2nd.css";
+import Header3rd from "./Header3rd";
 
 type HeaderProps = {
   children?: ReactNode;
 };
 
 const Header: React.FC<HeaderProps> = ({ children }) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsDrawerOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsDrawerOpen(false);
+  };
+
   return (
     <div className="second-header-container">
       <div className="second-header-fontstyle">
-        <div className={"second-header-top-bar"}>
+        <div
+          className={`second-header-top-bar ${isDrawerOpen ? "drawer-open" : ""}`}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
           <div className="second-header-btn-container">
-            <Link className="second-header-link" to={"/productList"}>
+            <Link className="second-header-link" to={"/productList/all"}>
               <MenuIcon />
               <p>카테고리</p>
             </Link>
@@ -29,9 +44,12 @@ const Header: React.FC<HeaderProps> = ({ children }) => {
               이벤트
             </Link>
           </div>
+          <div className={`second-header-drawer ${isDrawerOpen ? "drawer-open" : ""}`}>
+            <Header3rd />
+          </div>
         </div>
+        {children}
       </div>
-      {children}
     </div>
   );
 };
---
+++ src/layout/navigation/Header3rd.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { CategoryType } from "./entity/CategoryType";
+
+const categoryInfo: CategoryType[] = [
+  {
+    categoryId: 1,
+    categoryName: { name: "재배방식", categoryName: "cultivationmethod" },
+    categoryElements: {
+      organic: { name: "유기농", elementName: "ORGANIC" },
+      pesticidefree: { name: "무농약", elementName: "PESTICIDE_FREE" },
+      environmentfriendly: { name: "친환경", elementName: "ENVIRONMENT_FRIENDLY" },
+    },
+  },
+  {
+    categoryId: 2,
+    categoryName: { name: "생산지", categoryName: "region" },
+    categoryElements: {
+      GYEONGI: { name: "경기", elementName: "경기" },
+      GANGWON: { name: "강원", elementName: "강원" },
+      CHUNGBUK: { name: "충북", elementName: "충북" },
+      CHUNGNAM: { name: "충남", elementName: "충남" },
+      GYEONGBUK: { name: "경북", elementName: "경북" },
+      GYEONGNAM: { name: "경남", elementName: "경남" },
+      JEONBUK: { name: "전북", elementName: "전북" },
+      JEONNAM: { name: "전남", elementName: "전남" },
+      JEJU: { name: "제주", elementName: "제주" },
+    },
+  },
+];
+
+const CategoryComponent = ({ category }: { category: CategoryType }) => {
+  return (
+    <div>
+      <div>{category.categoryName.name}</div>
+      <ul>
+        {Object.keys(category.categoryElements).map((key) => (
+          <li key={key}>{category.categoryElements[key].name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Header3rd = () => {
+  return (
+    <div>
+      {categoryInfo.map((category) => (
+        <CategoryComponent key={category.categoryId} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Header3rd;
