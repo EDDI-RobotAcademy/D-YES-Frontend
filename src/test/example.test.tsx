a66d8acb23c90c002f3d41b76a45c06301d51cd9
jeongdawun <jeongdawun33@gmail.com>
2023-09-12 01:24:28 +0900

    feat: [정다운] 사용자가 메인페이지에서 농산물 가격을 테이블, 차트로 확인할 수 있는 기능 [RDY-105]

--- src/test/example.test.tsx
+++ src/test/example.test.tsx
@@ -1,16 +1,42 @@
 import React from "react";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 import "matchmedia-polyfill";
 import "matchmedia-polyfill/matchMedia.addListener";
-import { render } from "@testing-library/react";
-import { HashRouter } from 'react-router-dom';
+import { render, waitFor } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import MainPage from "../page/MainPage";
+import * as FarmProduceApi from "page/farmProduce/api/FarmProduceApi";
 
+jest.mock("../page/farmProduce/api/FarmProduceApi", () => ({
+  getFarmProducePriceList: jest.fn(),
+}));
 
-it("메인페이지 테스트", () => {
-    render(
-        <HashRouter>
-            <MainPage />
-        </HashRouter>
-    );
+it("메인페이지 테스트", async () => {
+  const farmProducePriceList = [
+    {
+      farmProduceName: "cabbage",
+      priceList: [
+        { "2023-09-11": 1366 },
+        { "2023-09-12": 1354 },
+        { "2023-09-13": 1253 },
+        { "2023-09-14": 1354 },
+        { "2023-09-15": 1254 },
+        { "2023-09-16": 1253 },
+        { "2023-09-17": 1264 },
+      ],
+    },
+  ];
+
+  (FarmProduceApi.getFarmProducePriceList as jest.Mock).mockResolvedValue(
+    farmProducePriceList
+  );
+  const { getByText } = render(
+    <BrowserRouter>
+      <MainPage />
+    </BrowserRouter>
+  );
+
+  await waitFor(() => {
+    expect(getByText("양배추")).toBeInTheDocument();
+  });
 });
