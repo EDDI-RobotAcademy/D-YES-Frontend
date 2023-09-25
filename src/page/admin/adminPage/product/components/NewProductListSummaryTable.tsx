9c1797c110a8f1b39f72a08280787c097ef9a0e6
jeongdawun <jeongdawun33@gmail.com>
2023-09-25 17:36:51 +0900

    style: [정다운] 관리자 대쉬보드 신규 상품 목록 테이블 스타일 변경 [RDY-157]

--- src/page/admin/adminPage/product/components/NewProductListSummaryTable.tsx
+++ src/page/admin/adminPage/product/components/NewProductListSummaryTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   TableContainer,
   Table,
@@ -8,7 +8,10 @@ import {
   TableRow,
   TableCell,
   Chip,
+  IconButton,
 } from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import { styled } from "@mui/material/styles";
 import { tableCellClasses } from "@mui/material/TableCell";
 import { NewProductSummaryInfo } from "page/product/entity/NewProductSummaryInfo";
@@ -16,11 +19,12 @@ import "../css/NewProductListSummaryTable.css";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
+    backgroundColor: theme.palette.common.white,
     color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
+    border: 0,
   },
 }));
 
@@ -37,44 +41,70 @@ interface ChartFormProps {
   productList: NewProductSummaryInfo[];
 }
 
-const NewProductListSummaryTable: React.FC<ChartFormProps> = ({
-  productList,
-}) => {
+const ITEMS_PER_PAGE = 5;
+
+const NewProductListSummaryTable: React.FC<ChartFormProps> = ({ productList }) => {
+  const [page, setPage] = useState(1);
+
+  const handleChangePage = (newPage: number) => {
+    setPage(newPage);
+  };
+
+  const displayData = productList.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE);
+
   return (
     <div className="product-info-container">
       <div className="new-product-list">
-        <h3>Recent Products</h3>
+        <h4>신규 등록 상품</h4>
         <div className="product-table">
           <TableContainer
+            className="product-table"
             component={Paper}
             style={{ width: "100%", fontSize: "8px" }}
           >
             <Table>
               <TableHead>
                 <TableRow>
-                  <StyledTableCell style={{ fontSize: "14px", width: "10%" }}>
+                  <StyledTableCell
+                    className="product-cell-style"
+                    style={{
+                      width: "10%",
+                    }}
+                  >
                     번호
                   </StyledTableCell>
                   <StyledTableCell
-                    style={{ fontSize: "14px", width: "30%" }}
+                    className="product-cell-style"
+                    style={{
+                      width: "30%",
+                    }}
                     align="center"
                   >
                     상품명
                   </StyledTableCell>
                   <StyledTableCell
-                    style={{ fontSize: "14px", width: "20%" }}
+                    className="product-cell-style"
+                    style={{
+                      width: "20%",
+                    }}
                     align="center"
                   >
                     판매상태
                   </StyledTableCell>
                   <StyledTableCell
-                    style={{ fontSize: "14px", width: "20%" }}
+                    className="product-cell-style"
+                    style={{
+                      width: "20%",
+                    }}
                     align="center"
                   >
                     농가
                   </StyledTableCell>
                   <StyledTableCell
-                    style={{ fontSize: "14px", width: "20%" }}
+                    className="product-cell-style"
+                    style={{
+                      width: "20%",
+                    }}
                     align="center"
                   >
                     등록일자
@@ -82,48 +112,25 @@ const NewProductListSummaryTable: React.FC<ChartFormProps> = ({
                 </TableRow>
               </TableHead>
               <TableBody>
-                {productList.map((row) => (
+                {displayData.map((row) => (
                   <StyledTableRow key={row.productId}>
-                    <StyledTableCell
-                      style={{ fontSize: "14px" }}
-                      component="th"
-                      scope="row"
-                    >
+                    <StyledTableCell className="product-cell-body-style" component="th" scope="row">
                       {row.productId}
                     </StyledTableCell>
-                    <StyledTableCell
-                      style={{ fontSize: "14px" }}
-                      align="center"
-                    >
+                    <StyledTableCell className="product-cell-body-style" align="center">
                       {row.productName}
                     </StyledTableCell>
-                    <StyledTableCell
-                      style={{ fontSize: "14px" }}
-                      align="center"
-                    >
+                    <StyledTableCell className="product-cell-body-style" align="center">
                       <Chip
-                        label={
-                          row.productSaleStatus === "AVAILABLE"
-                            ? "SALE"
-                            : "STOP"
-                        }
-                        color={
-                          row.productSaleStatus === "AVAILABLE"
-                            ? "success"
-                            : "error"
-                        }
+                        size="small"
+                        label={row.productSaleStatus === "AVAILABLE" ? "SALE" : "STOP"}
+                        color={row.productSaleStatus === "AVAILABLE" ? "success" : "error"}
                       />
                     </StyledTableCell>
-                    <StyledTableCell
-                      style={{ fontSize: "14px" }}
-                      align="center"
-                    >
+                    <StyledTableCell className="product-cell-body-style" align="center">
                       {row.farmName}
                     </StyledTableCell>
-                    <StyledTableCell
-                      style={{ fontSize: "14px" }}
-                      align="center"
-                    >
+                    <StyledTableCell className="product-cell-body-style" align="center">
                       {row.registrationDate.toString()}
                     </StyledTableCell>
                   </StyledTableRow>
@@ -131,6 +138,20 @@ const NewProductListSummaryTable: React.FC<ChartFormProps> = ({
               </TableBody>
             </Table>
           </TableContainer>
+          <div className="pagination">
+            <IconButton onClick={() => handleChangePage(page - 1)} disabled={page === 1}>
+              <NavigateBeforeIcon />
+            </IconButton>
+            <span>
+              Page {page} of {Math.ceil(productList.length / ITEMS_PER_PAGE)}
+            </span>
+            <IconButton
+              onClick={() => handleChangePage(page + 1)}
+              disabled={page === Math.ceil(productList.length / ITEMS_PER_PAGE)}
+            >
+              <NavigateNextIcon />
+            </IconButton>
+          </div>
         </div>
       </div>
     </div>
