5b18056ce1048466ee80a80e8cba96348a675d5e
bbbii <hyeongjin1326@gmail.com>
2023-10-06 10:04:32 +0900

    style: [김형진] Header 카테고리 스타일 수정 [RDY-80]

--- src/layout/navigation/Header2nd.tsx
+++ src/layout/navigation/Header2nd.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import "./css/Header2nd.css";
@@ -10,40 +10,51 @@ type HeaderProps = {
 };
 
 const Header: React.FC<HeaderProps> = ({ children }) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const showDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+    console.log(isDrawerOpen);
+  };
+
+  const closeDrawer = () => {
+    setIsDrawerOpen(false);
+  };
+
   return (
     <div className="second-header-container">
       <div className="second-header-fontstyle">
         <div className="second-header-top-bar">
           <div className="second-header-btn-container-1">
-            <Link className="second-header-link" to={"/productList/all"}>
+            <div className="second-header-element" onClick={showDrawer}>
               <MenuIcon />
-              <div>
-                <p>카테고리</p>
+              <p>카테고리</p>
+            </div>
+          </div>
+          {isDrawerOpen && (
+            <div className="second-header-drawer">
+              <Header3rd closeDrawer={closeDrawer} />
+            </div>
+          )}
+          <div className="second-header-btn-container-2">
+            <Link className="second-header-element" to={"/productList/all"}>
+              <div style={{ minWidth: "60px", textAlign: "center" }}>
+                <p>전체상품</p>
               </div>
             </Link>
-          </div>
-          <div className="second-header-drawer">
-            <Header3rd />
-          </div>
-          <div className="second-header-btn-container-2">
-            <Link className="second-header-link" to={"/productList/new"}>
+            <Link className="second-header-element" to={"/productList/new"}>
               <div style={{ minWidth: "60px", textAlign: "center" }}>
                 <p>신상품</p>
               </div>
             </Link>
-            <Link className="second-header-link" to={"/"}>
-              <div style={{ minWidth: "60px", textAlign: "center" }}>
-                <p>베스트</p>
-              </div>
-            </Link>
-            <Link className="second-header-link" to={"/event/list/all"}>
+            <Link className="second-header-element" to={"/event/list/all"}>
               <div style={{ minWidth: "60px", textAlign: "center" }}>
                 <p>이벤트</p>
               </div>
             </Link>
           </div>
           <div className="second-header-btn-container-3">
-            <Link className="second-header-link second-header-recipe-btn" to={"/recipe/list"}>
+            <Link className="second-header-element second-header-recipe-btn" to={"/recipe/list"}>
               <RestaurantIcon fontSize="small" />
               레시피
             </Link>
